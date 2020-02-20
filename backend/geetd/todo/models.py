a533993abc2bb8b2d9f378d8170a46c4eb58f741
Eric Honorez <eric.honorez@gmail.com>
2020-02-20 14:27:35 +0100

    Priority order added in the model

--- backend/geetd/todo/models.py
+++ backend/geetd/todo/models.py
@@ -1,6 +1,8 @@
 import uuid
 
 from django.db import models
+from django.db.models import Q
+
 from django.core.validators import MinLengthValidator
 from django.core.validators import MaxLengthValidator
 
@@ -25,3 +27,37 @@ class Todo(ValidateOnSaveMixin, models.Model):
     title = models.CharField(max_length=255, null=False, blank=False, validators=[MinLengthValidator(1), MaxLengthValidator(255)])
     is_done = models.BooleanField(null=False, default=False)
     state = models.CharField(max_length=5, choices=STATES, default=INBOX)
+    priority_order = models.IntegerField(default=0)
+
+    def move_to_state(self, state):
+        self.priority_order = Todo.get_next_priority_order(state)
+        self.state = state
+        self.save()
+    
+    def prioritize(self, priority_order):
+        #TODO: replace with one update query
+        todos = Todo.objects.filter(
+            ~Q(pk=self.id),
+            Q(state=self.state),
+            Q(priority_order__gte=priority_order)
+        ).order_by('priority_order')
+        
+        for idx, todo in enumerate(todos, start=1):
+            todo.priority_order += idx
+            todo.save()
+
+        self.priority_order = priority_order
+        self.save()
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            try:
+                self.priority_order = Todo.get_next_priority_order(state)
+            except:
+                pass              
+
+        super(Todo, self).save(*args, **kwargs)
+
+    @staticmethod
+    def get_next_priority_order(state):
+        Todo.objects.filter(state=state).order_by('-priority_order').first().priority_order + 1
