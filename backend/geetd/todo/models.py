eef5937e75b6186b06afb804d1e0916735cd044f
Eric Honorez <eric.honorez@gmail.com>
2020-02-20 11:33:00 +0100

    Todo state added

--- backend/geetd/todo/models.py
+++ backend/geetd/todo/models.py
@@ -4,7 +4,24 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 from django.core.validators import MaxLengthValidator
 
-class Todo(models.Model):
+from django.core.exceptions import ValidationError
+
+INBOX = 'inbox'
+NEXT = 'next'
+
+class ValidateOnSaveMixin():
+    def save(self, skip_validate=False, **kwargs):
+        if not skip_validate:
+            self.full_clean()
+        super(ValidateOnSaveMixin, self).save(kwargs)
+
+class Todo(ValidateOnSaveMixin, models.Model):
+    STATES = (
+        (INBOX, 'Inbox'),
+        (NEXT, 'Next')
+    )
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255, null=False, blank=False, validators=[MinLengthValidator(1), MaxLengthValidator(255)])
     is_done = models.BooleanField(null=False, default=False)
+    state = models.CharField(max_length=5, choices=STATES, default=INBOX)
--- backend/geetd/todo/views.py
+++ backend/geetd/todo/views.py
@@ -9,7 +9,7 @@ class TodoListView(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
-    filterset_fields = ['is_done']
+    filterset_fields = ['is_done', 'state']
 
 class TodoView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
