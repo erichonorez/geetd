589e8c7e7e52e725c80e73edcd5268b50fd5c1fc
Eric Honorez <eric.honorez@gmail.com>
2020-02-26 13:28:17 +0100

    Toggle complete a todo created

--- backend/geetd/todo/web/urls.py
+++ backend/geetd/todo/web/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name="web-todo-list")
+    path('', views.HomeView.as_view(), name="web-todo-list"),
+    path('todos/toggle-complete', views.ToggleCompleteView.as_view(), name='web-todo-toggle-complete')
 ]
--- backend/geetd/todo/web/views.py
+++ backend/geetd/todo/web/views.py
@@ -1,5 +1,9 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.views import View
+from django.urls import reverse
 
 from ..models import Todo
 from ..models import INBOX
@@ -12,8 +16,34 @@ class HomeView(View):
         todos = Todo.objects\
                     .filter(state=state)\
                     .order_by('priority_order')
-        return render(request, 'todo/index.html', {
-            'todos': todos,
-            'states': Todo.STATES,
-            'selected_state': state,
-        })
+        response = render(request, 'todo/index.html', {'todos': todos, 'states': Todo.STATES, 'selected_state': state, })
+        response.set_cookie('selected_state', state, httponly=True)
+        return response
+
+
+class ToggleCompleteForm(forms.Form):
+    todo_id = forms.UUIDField(required=True)
+
+
+class ToggleCompleteView(View):
+
+    def post(self, request):
+        form = ToggleCompleteForm(request.POST)
+        selected_state = request.COOKIES.get('selected_state', INBOX)
+        if not form.is_valid():
+            todos = Todo.objects \
+                .filter(state=selected_state) \
+                .order_by('priority_order')
+            response = render(request, 'todo/index.html', {
+                'todos': todos,
+                'states': Todo.STATES,
+                'selected_state': selected_state,
+                'toggleCompleteForm': form
+            })
+            return response
+
+        todo = get_object_or_404(Todo, pk=form.cleaned_data['todo_id'])
+        todo.is_done = not todo.is_done
+        todo.save()
+
+        return HttpResponseRedirect(reverse('web-todo-list') + '?state=' + selected_state)
